b26f0b038290b5363de9ba3da28f08ac55db81e2
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-05-22 01:30:24 +0700

    styling Nav

--- src/parts/Nav.js
+++ src/parts/Nav.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Nav = () => {
-    return (
-        <div className="nav">
-           <span>MovieApp</span>
-           <img
-           src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-           alt="Avatar profile"
-           />
-        </div>
-    )
-}
+  const [show, handleShow] = useState(false);
 
-export default Nav
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <span className="nav__logo">MovieApp</span>
+
+      <img
+        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        alt="Avatar profile"
+        className="nav__avatar"
+      />
+    </div>
+  );
+};
+
+export default Nav;
