ec94140aea035571dba95aaa453daa5f53c895a5
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-05-21 21:41:23 +0700

    fixing Row.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import { Row } from './parts'
+import React from "react";
+import { Row } from "./parts";
+import requests from "./config/request";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="App"> 
-      <Row title="NETFLIX ORIGINALS"/>
-      <Row title="Trending now"/>
+    <div className="App">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fecthNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fecthTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fecthTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fecthActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fecthComedyMovies} />
+      <Row title="Horor Movies" fetchUrl={requests.fecthHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fecthRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fecthDocumentaries} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/parts/Row.js
+++ src/parts/Row.js
@@ -1,12 +1,38 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect } from "react";
+import api from "../config/api";
 
-const Row = ({title}) => {
-    const [movies, setMovies] = useState([])
-    return (
-        <div>
-           <h2>{title}</h2>
-        </div>
-    )
-}
+const baseURL = "https://image.tmdb.org/t/p/original/";
 
-export default Row
+const Row = ({ title, fetchUrl,isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = await api.get(fetchUrl);
+      console.log(request.data.results);
+      setMovies(request.data.results);
+      return request;
+    };
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+          key={movie.id}
+            src={`${baseURL}${isLargeRow ? movie.poster_path: movie.backdrop_path}`}
+            alt={movie.name}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            title={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
