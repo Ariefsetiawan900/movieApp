f8125b55d0941d5c394086cbe88522e7adf8ca2f
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-05-23 17:32:39 +0700

    styling signup

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./style.css"
 
 const Signup = () => {
     return (
-        <div>
-            
+        <div className="signup">
+            <form>
+                <h1>Sign In</h1>
+                <input placeholder="email" type="email"/>
+                <input placeholder="password" type="password"/>
+                <button type="submit">Sign In</button>
+
+                <h4>
+                    <span className="signup__gray">New to MovieApp? </span>
+                    <span className="signup__link"> Sign Up now.</span>
+                    </h4>
+            </form>
         </div>
     )
 }
