f6d2877b7ca62b73f86e4b83d21000207caa3570
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-05-23 14:51:25 +0700

    styling login & create signup

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import Signup from '../Signup'
 import "./style.css";
 
 const Login = () => {
+  const [signIn, setSignIn] = useState(false);
   return (
     <div className="login">
       <div className="login__background">
         <span className="login__logo">MovieApp</span>
-        <button className="login__button">Sign In</button>
-        <div className="login__gradient"/>
+        <button onClick={() => setSignIn(true)} className="login__button">
+          Sign In
+        </button>
+        <div className="login__gradient" />
+      </div>
+
+      <div className="login__body">
+        {signIn ? (
+          <Signup />
+        ) : (
+          <>
+            <h1>Unlimited films, TV and more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </h3>
+
+            <div className="login__input">
+              <form>
+                <input type="email" placeholder="Email Addres" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="login__getStarted"
+                  type="submit"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
