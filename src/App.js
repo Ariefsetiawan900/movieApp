b867f27d683587e87c96210ed2ac782cec1b9760
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-05-24 12:45:31 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Hero, LandingPage, Profile, NotFound } from "./pages";
+import { Hero, LandingPage, Profile } from "./pages";
 
 const App = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
@@ -13,13 +13,11 @@ const App = () => {
         {!user ? (
           <Switch>
             <Route exact path="/" component={Hero} />
-            <Route exact path="*" component={NotFound} />
           </Switch>
         ) : (
           <Switch>
             <Route exact path="/landingpage" component={LandingPage} />
             <Route exact path="/profile" component={Profile} />
-            <Route exact path="*" component={NotFound} />
           </Switch>
         )}
       </Router>
